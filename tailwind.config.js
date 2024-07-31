66694306529ba4b11680218bdd0296ee382f735f
Dildora Muslimova <faithpacifica@yahoo.com>
2024-08-01 04:05:13 +0900

    feat:add tailwind features

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,25 @@ module.exports = {
         gotham: ['Gothampro', 'sans-serif'],
         gothamBold: ['GothamproBold', 'sans-serif'],
     },
+    backgroundImage: {
+      'hero-bg': "url('/src/assets/images/hero-bg.jpg')",
+    },
+    colors:{
+      orange:'#F57F05',
+      green:'#15BBC7'
+    },
+    fontSize: {
+      sm: '14px',
+      base: '16px',
+      lg: '18px',
+      xl: '1.25rem',
+    },
+    fontWeight: {
+      light: "300",
+      regular: "400",
+      medium: "500",
+      semibold: "600",
+    },
     },
   },
   plugins: [],
