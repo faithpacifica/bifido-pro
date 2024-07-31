ab509ba2249d59ad680c76af9b0228d1f7765060
Dildora Muslimova <faithpacifica@yahoo.com>
2024-08-01 03:33:33 +0900

    feat:add fonts images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Navbar from './components/Navbar';
-import './index.css';  // or wherever you placed the CSS file
+import './index.css';  
+
 import { useTranslation } from 'react-i18next';
 
 function App() {
   const { t } = useTranslation();
   return (
-    <div className="text-red-600 font-bold">
+    <div className=" font-gotham">
       <Navbar />
       <h1>{t('welcome')}</h1>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './assets/fonts/gothampro-black.ttf'
 import App from './App';
 import './i18n'; 
 
