e0120c3237f1b4d1c245bfbc827feca275140389
Guli-Alimova <86463317+Guli-Alimova@users.noreply.github.com>
2024-08-02 01:45:01 +0500

    style:styled about us

---
+++ src/components/AnimationButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AnimationButton = ({ customClass, children, customLink }) => {
+    return (
+        <a className={`${customClass}`} href={customLink}>
+      
+            {children}
+        </a>
+    )
+}
+
+export default AnimationButton
