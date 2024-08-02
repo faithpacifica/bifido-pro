3c9f765fb2d0c7af34bb76a85dcaddcaad0fda14
Dildora Muslimova <faithpacifica@yahoo.com>
2024-08-03 03:46:25 +0900

    feat:update Description sections

--- src/components/DescriptionOne.jsx
+++ src/components/DescriptionOne.jsx
@@ -1,31 +1,40 @@
 import React from 'react'
 import BubblesGreen from '../assets/images/bubbles-green.png';
 import medicineGreen from '../assets/images/sashe-green.png';
+import { DescriptionShape } from './DescriptionShape';
+import capsula from '../assets/images/capsula.png';
 
 const DescriptionOne = () => {
     return (
-      <section className='description-one'>
-          <div className="container">
-              <div className="flex desc-one-inner">
-                        <div className=' max-w-[50%] w-full py-12 '>
-                            <p>Дисбактериоз - нарушение нормальной микрофлоры </p>
-                            <div>
-                                <img src={BubblesGreen} className="" alt='intestine and bubbles' />
+        <section className='description-one'>
+            <div className="container">
+                <div className="flex desc-one-inner">
+                    <div className=' max-w-[50%] w-full py-12 '>
+                        <p>Дисбактериоз - нарушение нормальной микрофлоры </p>
+                        <div>
+                            <img src={BubblesGreen} className="" alt='intestine and bubbles' />
+                        </div>
+                    </div>
+                    <div className=' max-w-[50%] w-full'>
+                        <DescriptionShape
+                            imgCustomClass="w-[50px] h-[70px]"
+                            customClass=" bg-green border-orange"
+                            customImage={capsula}
+                            imgDesc="capsule"
+                            koeNumber="100"
+                            capsule="капсула"
+                        />
+                        <p>Применение Bifido PRO поможет восстановить нормальную микрофлору кишечника и избавит от симптомов дисбактериоза</p>
+                        <div className="flex right-bottom">
+                            <img src={medicineGreen} className="" alt='medicine' />
+                            <div className="flex flex-col buttons">
+                                {/* 2 ta button */}
                             </div>
                         </div>
-                        <div className=' max-w-[50%] w-full'>
-                            {/* yashil shakl */}
-                            <p>Применение Bifido PRO поможет восстановить нормальную микрофлору кишечника и избавит от симптомов дисбактериоза</p>
-                            <div className="flex right-bottom">
-                                <img src={medicineGreen} className="" alt='medicine' />
-                                <div className="flex flex-col buttons">
-                                    {/* 2 ta button */}
-                                </div>
-                            </div>
-                        </div>
-              </div>
-          </div>
-      </section>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
--- src/components/DescriptionTwo.jsx
+++ src/components/DescriptionTwo.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import BubblesOrange from '../assets/images/bubbles-orange.png';
 import medicineOrange from '../assets/images/sashe-orange.png';
+import { DescriptionShape } from './DescriptionShape';
+import sashe from '../assets//images/sashe.png';
+
 const DescriptionTwo = () => {
 
     return (
@@ -15,7 +18,14 @@ const DescriptionTwo = () => {
                         </div>
                     </div>
                     <div className=' max-w-[50%] w-full'>
-                        {/* oranjeviy shakl */}
+                        <DescriptionShape
+                            imgCustomClass=" h-[55px]"
+                            customClass=" bg-orange border-green"
+                            customImage={sashe}
+                            imgDesc="capsule"
+                            koeNumber="2"
+                            capsule="Саше"
+                        />
                         {/* TODO:tilga uzgaritirish keyin */}
                         <p>Применение Bifido PRO поможет восстановить нормальную микрофлору кишечника и снижает газообразования
                             При совместном применение с симетиконом быстро избавит от симптомов кишечной колики</p>
