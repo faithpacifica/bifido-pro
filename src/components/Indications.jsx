5cc1add3b4d677b55ce3c72ac81e03a0287cd4ab
AbdugafurovaUmida <abdugafurova93@gmail.com>
2024-08-03 03:12:50 +0500

    feat: cretae subscribe and footer component

--- src/components/Indications.jsx
+++ src/components/Indications.jsx
@@ -5,7 +5,7 @@ import AnimationButton from './AnimationButton';
 
 const Indications = () => {
     return (
-        <section className='py-12 bg-[url(./assets/images/bg-gray.png)]'>
+        <section className='py-12 bg-[url(./assets/images/bg-gray.png)] border-t-8 border-t-orange'>
             <h3 className='py-2 pr-10 bg-orange  text-white rounded-r-[50px] w-[350px] text-right text-3xl'>Как принимать</h3>
             <div className='container'>
                 <div className='flex justify-between gap-20'>
--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,20 +1,35 @@
 import React from 'react'
 import LogoSubscribe from '../assets/images/Untitled-2.png'
+import TelegramIcon from '../assets/images/telegram.png'
+import EarthIcon from '../assets/images/earth-globe.png'
 
 const Subscribe = () => {
     return (
-        <section className='py-12 bg-[url(./assets/images/bg-gray.png)]'>
+        <section className='py-12 bg-[url(./assets/images/bg-gray.png)] border-t-8 border-t-green'>
             <div className='flex items-center gap-20'>
                 <h3 className='py-2 pr-10 bg-orange  text-white rounded-r-[50px] w-[350px] text-right text-3xl'>Где купить?</h3>
                 <p className='text-xl'>Страница на стадии разработки.</p>
             </div>
-            <div>
-                <div></div>
-                <div>
-                    <img className='w-[400px]' src={LogoSubscribe} alt="" />
+            <div className='container'>
+                <div className=' flex justify-between items-center mt-10'>
+                    <div>
+                        <ul>
+                            <li className='flex items-center gap-5 mb-5'>
+                                <img className='w-[35px] h-[35px] rounded-full border-[2px] border-solid border-white' src={TelegramIcon} alt="telegram-icon" />
+                                <a className='text-2xl' href="https://t.me/arzonaptekabot" target='_blank' rel="noreferrer">https://t.me/arzonaptekabot </a>
+                            </li>
+                            <li className='flex items-center gap-5'>
+                                <img className='w-[35px] h-[35px] rounded-full border-[2px] border-solid border-transparent' src={EarthIcon} alt="earth-icon" />
+                                <a className='text-2xl' href="https://arzonapteka.uz/ru" target=' _blank' rel="noreferrer">https://arzonapteka.uz/ru </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <img className='w-[400px]' src={LogoSubscribe} alt="logo" />
+                    </div>
                 </div>
             </div>
-        </section>
+        </section >
     )
 }
 
