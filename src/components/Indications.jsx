7743bad9eda5f7dc73a3b1c9a0c16284e2ba78a8
AbdugafurovaUmida <abdugafurova93@gmail.com>
2024-08-02 10:27:20 +0500

    fix: change width contents

--- src/components/Indications.jsx
+++ src/components/Indications.jsx
@@ -6,20 +6,20 @@ import AnimationButton from './AnimationButton';
 const Indications = () => {
     return (
         <section className='py-12'>
-            <h3 className='py-2 pr-10 bg-orange  text-white rounded-r-[50px] w-[350px] text-right'>Как принимать</h3>
+            <h3 className='py-2 pr-10 bg-orange  text-white rounded-r-[50px] w-[350px] text-right text-3xl'>Как принимать</h3>
             <div className='container'>
-                <div className='flex'>
-                    <div className='pl-8 mt-8'>
-                        <img className='max-w-[55%] w-full' src={medicineGreen} alt="immuno-medicine" />
-                        <img className='max-w-[55%] w-full' src={medicineOrange} alt="babies-medicine" />
-                        <AnimationButton customClass=" rounded-full font-semibold px-6 bg-orange mt-8 inline-block text-white  border-2 border-green ">Разрешен с рождения</AnimationButton>
+                <div className='flex justify-between gap-20'>
+                    <div className='pl-10 mt-10 basis-[28%]'>
+                        <img className=' w-full' src={medicineGreen} alt="immuno-medicine" />
+                        <img className=' w-full' src={medicineOrange} alt="babies-medicine" />
+                        <AnimationButton customClass=" rounded-full font-semibold px-6 bg-orange mt-8 inline-block text-white  border-2 border-green text-lg">Разрешен с рождения</AnimationButton>
                     </div>
-                    <div>
-                        <h4 className='font-semibold text-3xl mb-2 text-gray'>Bifido PRO</h4>
+                    <div className='basis-[70%]'>
+                        <h4 className='font-semibold text-3xl mb-2 text-gray'>Bifido PRO <span className='!text-2xl'>капсула:</span></h4>
                         <table>
                             <tr>
-                                <th className='bg-green border-[2px] border-solid border-gray text-white'>Возраст</th>
-                                <th className='bg-green border-[2px] border-solid border-gray text-white'>Дозировка</th>
+                                <th className='bg-green border-[2px] border-solid border-gray text-white text-xl'>Возраст</th>
+                                <th className='bg-green border-[2px] border-solid border-gray text-white text-xl'>Дозировка</th>
                             </tr>
                             <tr>
                                 <td className='bg-transparent border-[2px] border-solid border-gray pl-2'>Взрослым и детям старше 12 лет:</td>
@@ -33,8 +33,8 @@ const Indications = () => {
                         <p className='bg-gray text-white pl-4 w-full mt-3'>Капсулы - внутрь, до еды. Рекомендуемый курс  приёма - 10 дней</p>
                         <table className='mt-20'>
                             <tr>
-                                <th className='bg-orange border-[2px] border-solid border-gray text-white'>Возраст</th>
-                                <th className='bg-orange border-[2px] border-solid border-gray text-white'>Дозировка</th>
+                                <th className='bg-orange border-[2px] border-solid border-gray text-white text-xl'>Возраст</th>
+                                <th className='bg-orange border-[2px] border-solid border-gray text-white text-xl'>Дозировка</th>
                             </tr>
                             <tr>
                                 <td className='bg-transparent border-[2px] border-solid border-gray pl-2'>С рождения до 6 месяцев:</td>
