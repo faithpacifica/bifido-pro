4f1b0cad00fffb5fb8745e212ae2f2a4c80ab17c
Dildora Muslimova <faithpacifica@yahoo.com>
2024-08-20 21:42:35 +0900

    add uzb translation to aboutSection

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,5 +1,6 @@
 
 import React from 'react'
+import { useTranslation } from 'react-i18next';
 import Image2 from "../assets/images/sashe-green.png"
 import Image3 from "../assets/images/sashe-orange.png"
 import Image from '../assets/images/rombik.png'
@@ -7,49 +8,51 @@ import Image1 from '../assets/images/pic2-removebg-preview.png'
 import AnimationButton from './AnimationButton'
 
 const AboutUs = () => {
+  const { t } = useTranslation();
+  
   return (
     <div className="border-b-8 border-b-green h-full py-[40px] md:pt-[80px] md:pb-[60px] about-bg ">
       <h3 className='py-2 px-3 md:px-5 md:px-[45px] bg-orange text-white rounded-r-[50px] w-full w-[400px] md:w-[450px] font-bold leading-relaxed text-lg md:text-2xl text-center md:text-left'>
-        Откройте "Мир - семейного прибиотика
+       
       </h3>
       <div className="container px-4 mx-auto md:mt-4 md:px-0">
         <div className='flex flex-col items-center justify-between space-y-8 md:flex-row md:items-start md:space-y-0'>
           <div className=' mt-8 text-black font-bold max-w-full md:max-w-[380px] leading-normal text-base md:text-xl  md:mt-5'>
             <div  data-aos="fade-left" data-aos-duration="1500"  className='flex items-start text-left'>
               <img src={Image} className='w-5 h-5 mt-2 mr-3' alt="rombik" />
-              <p>Пробиотик на основе живых бактерий</p>
+              <p> {t('aboutText1')}</p>
             </div>
             <div data-aos="fade-left" data-aos-duration="1100"  className='flex items-start text-left'>
               <img src={Image} className='w-5 h-5 mt-2 mr-3' alt="rombik" />
-              <p>Нормализует микрофлору кишечника</p>
+              <p>{t('aboutText2')}</p>
             </div>
             <div data-aos="fade-left" data-aos-duration="1300"  className='flex items-start text-left'>
               <img src={Image} className='w-5 h-5 mt-2 mr-3' alt="rombik" />
-              <p>Уничтожает рост и размножение патогенных бактерий</p>
+              <p>{t('aboutText3')}</p>
             </div>
             <div data-aos="fade-left" data-aos-duration="1400"  className='flex items-start text-left'>
               <img src={Image} className='w-5 h-5 mt-2 mr-3' alt="rombik" />
-              <p>Повышает сопротивляемость организма к кишечным и дыхательным инфекциям</p>
+              <p>{t('aboutText4')}</p>
             </div>
             <div data-aos="fade-left" data-aos-duration="1500"  className='flex items-start text-left'>
               <img src={Image} className='w-5 h-5 mt-2 mr-3' alt="rombik" />
-              <p>Улучшает усвояемость питательных веществ</p>
+              <p>{t('aboutText5')}</p>
             </div>
             <AnimationButton customClass="inline-flex items-center hover:bg-opacity-75 transition duration-300  rounded-full text-center px-3 py-2 bg-[#0EB1BA] mt-[54px] inline-block text-white font-normal border-2 border-[#DD9043]">
              {/* TODO:link */}
-              <a href="/"  target="_blank">О Бифидо ПРО</a>
+              <a href="/"  target="_blank">{t('aboutButton')}</a>
             </AnimationButton>
           </div>
           <div >
             <img src={Image1} className=' w-auto lg:w-[500px] md:mt-[-100px] lg:mt-[-282px] hidden lg:block' alt="family" />
           </div>
           <div className='flex flex-col items-center space-y-8 md:items-end mr-[42px] mt-[-33px]'>
-            <img  data-aos="zoom-in" data-aos-duration="1100"  src={Image3} className='w-[70%] sm:w-[50%] md:w-[200px]' alt="Бифидо про КАПС." />
             <img  data-aos="zoom-in" data-aos-duration="1100" src={Image2} className='w-[70%] sm:w-[50%] md:w-[200px] mb-[60px]' alt="medicine" />
+            <img  data-aos="zoom-in" data-aos-duration="1100"  src={Image3} className='w-[70%] sm:w-[50%] md:w-[200px]' alt="Бифидо про КАПС." />
             
-            <AnimationButton customClass=" hover:bg-opacity-75 transition duration-300 w-full text-lg rounded-full text-center py-1  px-3 bg-[#0EB1BA] inline-block text-white font-normal border-2 border-[#DD9043]">
+            <AnimationButton customClass=" hover:bg-opacity-75 transition duration-300 w-full text-lg rounded-full text-center py-1  px-3 bg-[#0EB1BA] inline-block text-white font-normal capitalize border-2 border-[#DD9043]">
              {/* TODO:link */}
-            <a href="/"  target="_blank">  ИНСТРУКЦИЯ</a>
+            <a href="/"  target="_blank"> {t('instruction')} </a>
             </AnimationButton>
           </div>
         </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,30 +1,3 @@
-// import React from 'react';
-// import { useTranslation } from 'react-i18next';
-// import medicineGreen from '../assets/images/sashe-green.png'
-// import medicineOrange from '../assets/images/sashe-orange.png'
-// export const Hero = () => {
-//     const { t } = useTranslation();
-//     return (
-//         <section className='border-t-8 border-b-8 border-b-green border-t-gray hero-bg'>
-//             <div className="container flex items-center justify-start ">
-//                 <div className="flex flex-col hero-left pt-16 max-w-[50%] w-full">
-//                     <h1 className='mb-16 text-5xl '>{t('heroTitle')}</h1>
-
-//                     <button className='inline-flex px-4 py-3 text-2xl text-white rounded-full w-fit align-left bg-orange' type="button">
-//                         {t('heroButton')}
-//                     </button>
-
-//                     <div className="flex justify-end  gap-10 mt-16 hero-image max-w-[70%]">
-//                         <img src={medicineGreen} className="max-w-[40%]" alt='medicine' />
-//                         <img src={medicineOrange} className="max-w-[40%]" alt='medicine' />
-//                     </div>
-//                 </div>
-//             </div>
-//             <div className="hero-right max-w-[50%] w-full"></div>
-
-//         </section >
-//     )
-// }
 
 
 import React from 'react';
