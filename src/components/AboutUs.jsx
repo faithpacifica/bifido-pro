8933d5cc1d52f26d915c46776ea04f3b52450e35
Dildora Muslimova <faithpacifica@yahoo.com>
2024-08-20 22:15:49 +0900

    fix:translation.json

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -13,7 +13,7 @@ const AboutUs = () => {
   return (
     <div className="border-b-8 border-b-green h-full py-[40px] md:pt-[80px] md:pb-[60px] about-bg ">
       <h3 className='py-2 px-3 md:px-5 md:px-[45px] bg-orange text-white rounded-r-[50px] w-full w-[400px] md:w-[450px] font-bold leading-relaxed text-lg md:text-2xl text-center md:text-left'>
-       
+      {t('aboutTitle')}
       </h3>
       <div className="container px-4 mx-auto md:mt-4 md:px-0">
         <div className='flex flex-col items-center justify-between space-y-8 md:flex-row md:items-start md:space-y-0'>
@@ -44,13 +44,13 @@ const AboutUs = () => {
             </AnimationButton>
           </div>
           <div >
-            <img src={Image1} className=' w-auto lg:w-[500px] md:mt-[-100px] lg:mt-[-282px] hidden lg:block' alt="family" />
+            <img src={Image1} className=' w-auto lg:w-[500px] md:mt-[-100px] lg:mt-[-242px] hidden lg:block' alt="family" />
           </div>
           <div className='flex flex-col items-center space-y-8 md:items-end mr-[42px] mt-[-33px]'>
-            <img  data-aos="zoom-in" data-aos-duration="1100" src={Image2} className='w-[70%] sm:w-[50%] md:w-[200px] mb-[60px]' alt="medicine" />
-            <img  data-aos="zoom-in" data-aos-duration="1100"  src={Image3} className='w-[70%] sm:w-[50%] md:w-[200px]' alt="Бифидо про КАПС." />
+            <img  data-aos="zoom-in" data-aos-duration="1100" src={Image2} className='w-[70%] sm:w-[50%] md:w-[200px] mb-[10px]' alt="medicine" />
+            <img  data-aos="zoom-in" data-aos-duration="1100"  src={Image3} className='m-0 w-[70%] sm:w-[50%] md:w-[200px]' alt="Бифидо про КАПС." />
             
-            <AnimationButton customClass=" hover:bg-opacity-75 transition duration-300 w-full text-lg rounded-full text-center py-1  px-3 bg-[#0EB1BA] inline-block text-white font-normal capitalize border-2 border-[#DD9043]">
+            <AnimationButton customClass=" hover:bg-opacity-75 transition duration-300 w-full text-lg rounded-full text-center py-1  px-3 bg-[#0EB1BA] inline-block text-white font-normal capitalise border-2 border-[#DD9043]">
              {/* TODO:link */}
             <a href="/"  target="_blank"> {t('instruction')} </a>
             </AnimationButton>
