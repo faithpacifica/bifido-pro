fbd934b305c2bc3df40581c81da44acb36574747
AbdugafurovaUmida <abdugafurova93@gmail.com>
2024-08-23 21:27:25 +0500

    fix: language context

--- src/contexts/LanguageContext.jsx
+++ src/contexts/LanguageContext.jsx
@@ -1,21 +1,25 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 
 export const LanguageContext = createContext();
 
 export const LanguageProvider = ({ children }) => {
-    const [language, setLanguage] = useState(localStorage.getItem('language') || 'en');
+    const [language, setLanguage] = useState(localStorage.getItem('language') || 'ru');
+    const { i18n } = useTranslation();
 
     const changeLanguage = (newLanguage) => {
         setLanguage(newLanguage);
         localStorage.setItem('language', newLanguage);
+        i18n.changeLanguage(newLanguage);
     };
 
     useEffect(() => {
         const storedLanguage = localStorage.getItem('language');
         if (storedLanguage) {
             setLanguage(storedLanguage);
+            i18n.changeLanguage(storedLanguage);
         }
-    }, []);
+    }, [i18n]);
 
     return (
         <LanguageContext.Provider value={{ language, changeLanguage }}>
